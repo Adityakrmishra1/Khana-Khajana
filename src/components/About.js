64fe070d3d5182cec20da3f9ee1f356d4d988d37
Aditya Kumar Mishra <adityakrmishraa@gmail.com>
2023-07-16 13:37:35 +0530

    User Class Updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import User from "./User";
+import UserClass from "./UserClass";
 
 const About = function () {
   return (
     <div className="about-container">
       <h1> 😎 About Me.. 😎</h1>
       <User />
+      <UserClass />
     </div>
   );
 };
