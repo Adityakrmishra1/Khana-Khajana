cd37a07a99c48aa2db38276ed6f921c75ba5e628
Aditya Kumar Mishra <adityakrmishraa@gmail.com>
2023-07-18 14:19:05 +0530

    Refactoring of the code

--- src/components/RestaurantsItem.js
+++ src/components/RestaurantsItem.js
@@ -1,28 +1,30 @@
 import { CDN_URL } from "../../utils/constants";
 
 let RestaurantsItem = function (props) {
-  console.log("props", props.data.data);
-  const name = props.data.data.name;
-  const avgRating = props.data.data.avgRating;
-  let cusines = props.data.data.cuisines;
-  const eta = props.data.data.deliveryTime;
-  const imageId = props.data.data.cloudinaryImageId;
-  const priceForTwo = props.data.data.costForTwo;
-  cusines = cusines.join(", ");
+  let {
+    name,
+    avgRating,
+    deliveryTime,
+    cuisines,
+    cloudinaryImageId,
+    costForTwo,
+  } = props.data.data;
+
+  cuisines = cuisines.join(", ");
   return (
     <div className="res-card-box" onMouseOver={() => {}}>
       <div className="res-item">
-        <img src={CDN_URL + imageId} />
+        <img src={CDN_URL + cloudinaryImageId} />
         <h4 className="res-item-title"> {name} </h4>
-        <h5 className="res-item-cusine"> {cusines} </h5>
+        <h5 className="res-item-cusine"> {cuisines} </h5>
         <div className="res-other-details">
           <span className="res-item-rating">
             <span className="star">&#9733;</span> {avgRating}
           </span>
           <div className="res-item-seperator">.</div>
-          <span className="res-item-eta"> {eta} Minutes </span>
+          <span className="res-item-eta"> {deliveryTime} Minutes </span>
           <div className="res-item-seperator">.</div>
-          <span> ₹{priceForTwo / 100} For Two</span>
+          <span> ₹{costForTwo / 100} For Two</span>
         </div>
         <div className="res-item-quick-view">
           <hr className="res-item-quick-view-hr"></hr>
