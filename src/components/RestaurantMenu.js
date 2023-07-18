9f1238b0e7dffc9a06444717c41fd98b3a9fc945
Aditya Kumar Mishra <adityakrmishraa@gmail.com>
2023-07-18 15:19:02 +0530

    Added Custom Hooks

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,26 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-import { RESTAURANT_MENU_URL } from "../../utils/constants";
-import ResturantTitle from "./ResturantTitle";
 import ResturantMenuListBody from "./ResturantMenuListBody";
+import useRestaurantMenu from "../../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resMenuData, setResMenuData] = useState([]);
   const { resId } = useParams();
+  // custom hooks
 
-  useEffect(() => {
-    fetchRestaurants();
-  }, []);
-  const fetchRestaurants = async () => {
-    const response = await fetch(RESTAURANT_MENU_URL + "/" + resId);
-    const data = await response.json();
-    setResMenuData(data?.data?.cards);
-  };
+  const resMenuData = useRestaurantMenu(resId);
+  // console.log(resMenuData);
+
   return (
     <div className="res-menu-container">
       {/* <ResturantTitle props={resMenuData[0]} /> */}
-      <ResturantMenuListBody props={resMenuData[2]} />
+      <ResturantMenuListBody props={resMenuData?.[2]} />
     </div>
   );
 };
---
+++ utils/useRestaurantMenu.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { RESTAURANT_MENU_URL } from "./constants";
+
+const useRestaurantMenu = (resId) => {
+  const [resInfo, setResInfo] = useState(null);
+  useEffect(() => {
+    fetchData(resId);
+  }, []);
+
+  const fetchData = async (resId) => {
+    const response = await fetch(RESTAURANT_MENU_URL + "/" + resId);
+    const data = await response.json();
+    setResInfo(data?.data?.cards);
+  };
+  console.log(resInfo);
+  return resInfo;
+};
+
+export default useRestaurantMenu;
